c61598fab07a484760dbd7a8b3cbc3be4658e997
Kopiev Ivan <x@mustdev.ru>
2023-02-18 19:32:11 +0300

    fix TourView

--- StudentTourism/Screens/TourView/TourViewView.swift
+++ StudentTourism/Screens/TourView/TourViewView.swift
@@ -9,12 +9,37 @@ import UIKit
 
 class TourViewView: BaseView {
     
-    override func customizeUI() {
-        
-    }
+    @IBOutlet var roomCollection: RoomCollection!
+
     
-    override func render() {
-        
+    override func customizeUI() {
+        roomCollection.items = [Item(reuseId: "BannerCell"),Item(reuseId: "BannerCell"),Item(reuseId: "BannerCell"),Item(reuseId: "BannerCell") ]
+    
     }
 }
 
+class RoomCollection: CollectionView {
+    @IBOutlet var pageControl: UIPageControl!
+    
+    override func setUp() {
+        CellSize(widthScale: 0.9, aspectRatio: 1.17, insetX: 16).forCollectionView(collectionView, lineSpacing: 16, scrollDirection: .horizontal, withDecelerateFast: true)
+        pageControl.numberOfPages = items.count
+    }
+    
+    override func render(data: Items) {
+        super.render(data: data)
+        pageControl.numberOfPages = items.count
+        
+    }
+    
+    override func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        super.scrollViewWillEndDragging(scrollView, withVelocity: velocity, targetContentOffset: targetContentOffset)
+        guard let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout else { return }
+        let cellWidthIncludingSpacing = layout.itemSize.width + layout.minimumLineSpacing
+        let offset = targetContentOffset.pointee
+        let index = (offset.x + scrollView.contentInset.left) / cellWidthIncludingSpacing
+        let roundedIndex = round(index)
+        pageControl.currentPage = Int( roundedIndex)
+    }
+    
+}
