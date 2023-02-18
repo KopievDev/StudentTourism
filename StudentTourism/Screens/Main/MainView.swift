d37a720aa099235c2d51bca4eb44205da9078b7a
Kopiev Ivan <x@mustdev.ru>
2023-02-18 14:55:26 +0300

    add pageControl

--- StudentTourism/Screens/Main/MainView.swift
+++ StudentTourism/Screens/Main/MainView.swift
@@ -29,13 +29,30 @@ class MainView: BaseView {
 
 
 class BannerCollection: CollectionView {
-
-   override func setUp() {
-       CellSize(widthScale: 0.9, aspectRatio: 0.6, insetX: 16).forCollectionView(collectionView, lineSpacing: 16, scrollDirection: .horizontal, withDecelerateFast: true)
+    @IBOutlet var pageControl: UIPageControl!
+    
+    override func setUp() {
+        CellSize(widthScale: 0.9, aspectRatio: 0.6, insetX: 16).forCollectionView(collectionView, lineSpacing: 16, scrollDirection: .horizontal, withDecelerateFast: true)
+        pageControl.numberOfPages = items.count
     }
-
+    
+    override func render(data: Items) {
+        super.render(data: data)
+        pageControl.numberOfPages = items.count
+        
+    }
+    
+    override func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        super.scrollViewWillEndDragging(scrollView, withVelocity: velocity, targetContentOffset: targetContentOffset)
+        guard let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout else { return }
+        let cellWidthIncludingSpacing = layout.itemSize.width + layout.minimumLineSpacing
+        let offset = targetContentOffset.pointee
+        let index = (offset.x + scrollView.contentInset.left) / cellWidthIncludingSpacing
+        let roundedIndex = round(index)
+        pageControl.currentPage = Int( roundedIndex)
+    }
+    
 }
-
 
 class CityCollection: CollectionView {
 
